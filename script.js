ba4eaa1874b1703eca56d8e50ff22baf8d52d2a8
MatheusLucca <mat.lualves@gmail.com>
2022-02-06 16:22:42 -0300

    Create object theme

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let theme = document.getElementById('theme-switch')
+let toggle = document.getElementById('theme-switch')
 let body = document.querySelector("body")
 
 function load() {
@@ -10,25 +10,27 @@ function load() {
     }
     if (id == 1) {
         dark();
-        theme.checked = true;
+        toggle.checked = true;
     }
 }
 
-let dark = () => {
-    body.classList.add("dark")
-    body.classList.remove("light")
+const Theme = {
+    dark() {
+        body.classList.add("dark")
+        body.classList.remove("light")
+    },
+    light() {
+        body.classList.add("light")
+        body.classList.remove("dark")
+    }
 }
 
-let light = () => {
-    body.classList.add("light")
-    body.classList.remove("dark")
-}
-theme.addEventListener("click", (a) => {
-    if (theme.checked == true) {
+toggle.addEventListener("click", (a) => {
+    if (toggle.checked == true) {
         localStorage.setItem("id", "1")
-        dark()
+        Theme.dark()
     } else {
-        light()
+        Theme.light()
         localStorage.setItem("id", "0")
     }
 })
